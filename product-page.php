48729e865077160b1725c161e7f371d0a5a2d13c
Zenotorith <65653624+Zenotorith@users.noreply.github.com>
2021-03-17 14:25:11 +0700

    Update product-page.php

--- product-page.php
+++ product-page.php
@@ -15,7 +15,7 @@
                     require_once 'connectDB.php';
                     $con = connectDB(); 
                     $id = $_GET['id'];
-                    $query = "select * from products";
+                    $query = "SELECT * FROM products WHERE \"pdID\"=".$id;
                     $result = pg_query($con, $query);
                     $row = pg_fetch_assoc($result);              
                     ?>
